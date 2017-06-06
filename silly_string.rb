819f6e1cf8d3692eebddeaa65af8e9b7b327456e
Janice Kim <janicekim60@gmail.com>
2017-06-06 15:24:31 +0000

    finished silly string lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,15 @@
-#code your solutions here
-
-
-
+my_word = "legendary"
+puts my_word.upcase
+shout = "IM NOT SHOUTING"
+puts shout.downcase
+kloss = "Karlie Kloss"
+puts kloss.length
+puts "wow " + "mom"
+like= "i really like programming"
+puts like.capitalize
+a = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+puts a.gsub("a","o")
+chi = "Chicago"
+la = "Los Angeles"
+ny = "New York"
+sl = "St. Louis"
